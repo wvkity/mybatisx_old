142374e80e6e134252c6effe9a76c990f025862f
wvkity <wvkity@gmail.com>
2021-02-22 11:51:27 +0800

    * 去除导入多余类

--- mybatis-core/src/main/java/com/wvkity/mybatis/core/handler/TableHandler.java
+++ mybatis-core/src/main/java/com/wvkity/mybatis/core/handler/TableHandler.java
@@ -31,8 +31,6 @@ import org.apache.ibatis.session.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
