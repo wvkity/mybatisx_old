1f3f38d53005339c558d8fc1437feed5649d4527
wvkity <wvkity@gmail.com>
2021-02-24 22:45:23 +0800

    * Identity注解存在的bug

--- mybatis-annotation/src/main/java/com/wvkity/mybatis/annotation/Identity.java
+++ mybatis-annotation/src/main/java/com/wvkity/mybatis/annotation/Identity.java
@@ -15,12 +15,23 @@
  */
 package com.wvkity.mybatis.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 自增主键注解
  * @author wvkity
  * @created 2020-10-04
  * @since 1.0.0
  */
+@Inherited
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Identity {
 
     /**
