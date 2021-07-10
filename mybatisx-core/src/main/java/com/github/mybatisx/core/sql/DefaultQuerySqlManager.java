1bd97cbea9e37dc1c3024464edb0a74d211c6393
wvkity <wvkity@gmail.com>
2021-07-10 16:02:18 +0800

    * 添加License

--- mybatisx-core/src/main/java/com/github/mybatisx/core/sql/DefaultQuerySqlManager.java
+++ mybatisx-core/src/main/java/com/github/mybatisx/core/sql/DefaultQuerySqlManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, wvkity(wvkity@gmail.com).
+ * Copyright (c) 2020, wvkity(wvkity@gmail.com).
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
